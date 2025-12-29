cffb607916828f6cf79580ffa7d672255249ccaa
Mikael312 <damino898989@gmail.com>
2025-12-29 20:26:21 +0800

    Update ArcadeUiLib.lua

--- ArcadeUiLib.lua
+++ ArcadeUiLib.lua
@@ -72,7 +72,7 @@ local function createNotificationGui()
     NotificationGui.Name = "ArcadeNotificationGui"
     NotificationGui.ResetOnSpawn = false
     NotificationGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-    NotificationGui.Parent = LocalPlayer:WaitForChild("PlayerGui")
+    NotificationGui.Parent = game.CoreGui
 end
 
 -- ==================== UI VARIABLES ====================
